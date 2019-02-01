19aecf734e852a3a8e007325bdc4accb5b5c3ab1
kaiobrito <kaio.a7x@gmail.com>
2019-02-01 14:41:12 -0300

    Add api/docs endpoint

--- django/gdg_ultimate_fight/urls.py
+++ django/gdg_ultimate_fight/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
 
     path('api/', include('rest_framework.urls')),
     path('api/', include('todo.urls')),
+    path('api/docs', include_docs_urls(title='GDG Ultimate Fight')),
     path('admin/', admin.site.urls),
 ]
