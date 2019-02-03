8b8aecfba2ade292b99020869f96f461a7ba9db2
kaiobrito <kaio.a7x@gmail.com>
2019-02-03 08:50:16 -0300

    Add ASSIGN_TASK_TO_USER signal

--- django/todo/signals.py
+++ django/todo/signals.py
@@ -2,6 +2,18 @@ from django.core import signals
 from django.core.mail import send_mail
 
 CHANGE_TODO_STATUS = signals.Signal(providing_args=['todo_pk', 'status'])
+ASSIGN_TASK_TO_USER = signals.Signal(providing_args=['todo_pk', 'user_pk'])
+
+
+def assignUserToTask(todo_pk, user, **kwargs):
+    from .models import Todo
+    todo = Todo.objects.get(pk=todo_pk)
+    todo.assignees.add(user)
+
+    recipient_list = map(lambda user: user.email,
+                         todo.assignees.filter(email__isnull=False))
+    send_mail("New Task", "You were assigned to task {0}".format(
+        todo.title), "from@example.com", recipient_list)
 
 
 def onChangeTodoStatus(todo_pk, status, **kwargs):
@@ -19,3 +31,4 @@ def onChangeTodoStatus(todo_pk, status, **kwargs):
 
 def connect():
     CHANGE_TODO_STATUS.connect(onChangeTodoStatus)
+    ASSIGN_TASK_TO_USER.connect(assignUserToTask)
