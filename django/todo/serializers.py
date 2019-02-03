5e0725344c361611fec423b8320fb2f133c1b2e5
kaiobrito <kaio.a7x@gmail.com>
2019-02-03 08:50:38 -0300

    Add assign task to user endpoint

--- django/todo/serializers.py
+++ django/todo/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Todo
 
 
+class UserSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    username = serializers.ReadOnlyField()
+    email = serializers.ReadOnlyField()
+
+
 class TodoSerializer(serializers.ModelSerializer):
+    assignees = UserSerializer(many=True, read_only=True)
+
     class Meta:
         model = Todo
         fields = '__all__'
+
+
+class AssignSerializer(serializers.Serializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.filter(is_active=True))
--- django/todo/views.py
+++ django/todo/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import Serializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, AssignSerializer
+from .signals import ASSIGN_TASK_TO_USER
 
 
 class TodoViewSet(ModelViewSet):
@@ -10,6 +12,10 @@ class TodoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return self.request.user.todos.all()
+
+    def perform_create(self, serializer):
+        todo = serializer.save()
+        ASSIGN_TASK_TO_USER.send(self, todo_pk=todo.pk, user=self.request.user)
 
     @action(detail=True, methods=['post'], name='Mark it as todo', serializer_class=Serializer)
     def todo(self, request, pk):
@@ -28,3 +34,12 @@ class TodoViewSet(ModelViewSet):
         todo = self.get_queryset().get(pk=pk)
         todo.done()
         return Response(status=204)
+
+    @action(detail=True, methods=['post'], serializer_class=AssignSerializer)
+    def assign(self, request, pk):
+        todo = self.get_queryset().get(pk=pk)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = User.objects.get(pk=serializer.data.get('user'))
+        ASSIGN_TASK_TO_USER.send(self, todo_pk=todo.pk, user=user)
+        return Response(status=201)
