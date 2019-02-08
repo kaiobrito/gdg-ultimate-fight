6270cacac265e1741bc2aef2cd4c2cad60c541e3
Tony Messias <tonyzrp@gmail.com>
2019-02-07 22:53:57 -0300

    Add assign users to todo tests

---
+++ laravel/tests/Feature/AssignUsersToTodoTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AssignUsersToTodoTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGuestsCannotAssignUsersToAnything()
+    {
+    }
+
+    public function testUsersMustExistToBeAssigned()
+    {
+    }
+
+    public function testCanAssignUsersToTodos()
+    {
+    }
+
+    public function testCanAssignMultipleUsersAtOnce()
+    {
+    }
+}
