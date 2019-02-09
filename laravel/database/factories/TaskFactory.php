4f85f2998a0b74d1f53dd22e630e824151184c7b
Tony Messias <tonyzrp@gmail.com>
2019-02-08 21:52:11 -0300

    Tweak the seeding and tasks factory

--- laravel/database/factories/TaskFactory.php
+++ laravel/database/factories/TaskFactory.php
@@ -6,7 +6,8 @@ $factory->define(App\Task::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
         'description' => $faker->paragraph,
-        'status' => 'todo',
+        'status' => $faker->randomElement(['todo', 'doing', 'done']),
         'user_id' => factory(\App\User::class),
+        'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
     ];
 });
